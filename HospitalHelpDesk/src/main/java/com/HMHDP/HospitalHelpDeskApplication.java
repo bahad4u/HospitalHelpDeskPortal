a1faf29dfcf4f76f62cf6bfd640b063f35a28d82
bahad4u <42751897+bahad4u@users.noreply.github.com>
2019-12-01 19:21:30 +0530

    New Branch creation

---
+++ HospitalHelpDesk/src/main/java/com/HMHDP/HospitalHelpDeskApplication.java
@@ -0,0 +1,13 @@
+package com.HMHDP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalHelpDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalHelpDeskApplication.class, args);
+	}
+
+}
